70ce25bc3196fe415bd06290240487bf7d8142e3
fabio alves rosa <faabioalvesrosa@gmail.com>
2022-04-13 00:02:52 -0300

    variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,43 @@
+let nome
+let idade
+
+console.log(typeof nome)
+console.log(typeof idade)
+// undefild , diz que não foi atribuido nenhum valor a variavel
+
+nome = prompt("qual é o seu nome?")
+idade = prompt("qual é a sua idade?")
+
+nome = "Fabio"
+idade = 29
+
+console.log(typeof nome)
+console.log(typeof idade)
+// agora sim e uma string e number
+
+console.log("Olá" , nome , "você tem" , idade , "anos.")
+
+// 2
+prompt("Você gosta de lasanha?")
+prompt("você torce para o fluminense?")
+prompt("você mora no Rio de Janeiro?")
+
+
+
+
+
+
+
+
+
+// 3 -
+let a = 10
+let b = 25
+
+
+
+
+
+
+
+
