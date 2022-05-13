90f92297f8e4ea034dfe083bf3164d2dd57a64ee
fabio alves rosa <faabioalvesrosa@gmail.com>
2022-05-12 22:03:51 -0300

    exercicios-falta14-revisao

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -23,12 +23,22 @@ function retornaArrayOrdenado(array) {
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-
+    numerosPares = []
+    array.map((item)=>
+    {if(item %2 === 0)
+        numerosPares.push(item)
+    })
+    return numerosPares
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+    numerosPares = []
+    array.map((item)=>
+    {if(item %2 === 0)
+        numerosPares.push(item * item)
+    })
+    return numerosPares
 }
 
 // EXERCÍCIO 06
@@ -54,36 +64,51 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+    let primeirosPares = []
+    for (let i = 0; primeirosPares.length < n; i++) {
+        if (i % 2 == 0) {
+            primeirosPares.push(i)
+        }
+    }
+    return primeirosPares
 }
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+    if(ladoA==ladoB && ladoB==ladoC){return "Equilátero";
+    }else if(ladoA!=ladoB && ladoB!=ladoC && ladoA!=ladoC){return "Escaleno"
+    }else{return"Isósceles";}
 }
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+    array.sort((a, b) => a -b)
+    return [array[array.length-2],array[1]]
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+   return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+   let array = {...pessoa, nome:"ANÔNIMO"}
+   return array
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+   return pessoas.filter((item)=> {
+       return (item.idade > 14 && item.idade <60 && item.altura > 1.5)
+   })
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
+    return pessoas.filter((item)=> {
+        return (item.idade <= 14 || item.idade >60 || item.altura <1.5)
+    })
   
 }
 
@@ -94,10 +119,26 @@ function retornaContasComSaldoAtualizado(contas) {
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+    function compare(a,b) {
+        if (a.nome < b.nome)
+           return -1;
+        if (a.nome > b.nome)
+          return 1;
+        return 0;
+      }
+      
+      return consultas.sort(compare);
+    
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-    
+        //function tentativa (a, b){
+        //if (a.dataDaConsulta < b.dataDaConsulta)
+          //   return -1;
+        //if (a.dataDaConsulta > b.dataDaConsulta)
+         //   return 1;
+         //   return 0;
+//}
+ //   return consultas.sort(tentativa);
 }
